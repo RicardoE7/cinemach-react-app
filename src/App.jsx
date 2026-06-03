e846579ca0cbee3e569b4e95047acd1464b6d850
Ricardo Edwards <ricardo.edwards4899@gmail.com>
2026-06-03 01:31:56 -0400

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const getApiOptions = () => ({
 
 const App = () => {
 
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm ] = useState('');
 
   const [movies, setMovies] = useState([]);
 
